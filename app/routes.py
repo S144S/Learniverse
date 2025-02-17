fdd14a2ed00292b7aaf98aeb957370812794666f
Saeed <saeed144.73@gmail.com>
2025-02-17 10:10:17 +0330

    🧾 Transfer template files

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,18 @@
 from app import app
 from flask import render_template
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('layout.html')
 
-@app.route('/mission')
-def mission():
-    return render_template('mission.html')
+# @app.route('/mission')
+# def mission():
+#     return render_template('mission.html')
 
-@app.route('/planet')
-def planet():
-    return render_template('planet.html')
+# @app.route('/planet')
+# def planet():
+#     return render_template('planet.html')
