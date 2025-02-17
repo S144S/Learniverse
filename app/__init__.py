7cfbf0f2742af998c9e58721e7984e9db416a989
Saeed <saeed144.73@gmail.com>
2025-02-17 18:11:17 +0330

    🧾 Register new user

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from .db_helper import DBHelper
 
 app = Flask(__name__)
+app.secret_key = "super_secret_key"
+
+db = DBHelper()
+    
+bcrypt = Bcrypt(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,31 @@
-from app import app
-from flask import render_template
+from app import app, db, bcrypt
+from flask import render_template, request, redirect, url_for, session, flash
 
-@app.route('/register')
+
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        fname = request.form["fname"]
+        lname = request.form["lname"]
+        grade = request.form["grade"]
+        username = request.form["username"]
+        password = request.form["password"]
+        if not fname or not lname or not grade or not username or not password:
+            flash("پر کردن تمام فیلدها اجباری هست!", "danger")
+            return redirect(url_for('register'))
+        re_password = request.form["reppass"]
+        if password != re_password:
+            flash("گذرواژه و تکرار گذرواژه یکسان نیست!", "danger")
+            return redirect(url_for('register'))
+        # Hash password
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        done = db.users.add_user(username, hashed_password, fname, lname, int(grade))
+        if done:
+            flash("ثبت‌نام با موفقیت انجام شد!", "success")
+            return redirect(url_for('login'))
+        else:
+            flash("خطا در ثبت‌نام، لطفا دوباره امتحان کنید.", "danger")
+            return redirect(url_for('register'))
     return render_template('register.html')
 
 @app.route('/login')
