820a28447635e0aa8bb5b402c801e94d776c1777
Saeed <saeed144.73@gmail.com>
2025-02-17 10:24:54 +0330

    🧾 Create register & login page

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,10 @@ from flask import render_template
 def register():
     return render_template('register.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/')
 def home():
     return render_template('layout.html')
